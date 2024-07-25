b5f40ff1c3c2e9bf13f97c17a72c0b483c65a58c
remirami <remiraz@gmail.com>
2024-07-25 21:50:45 +0300

    Asd

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "Hello, World!"
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello world</p>"
-
-if __name__ == 'main':
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
